cb383355b692c7df67d886699a64e8dd6098efad
Georgina Van Dort <GeorginaVanDort@gmail.com>
2016-08-09 15:57:10 -0700

    set up angular files

---
+++ app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component, EventEmitter } from 'angular2/core';
+
+
+
+@Component({
+  selector: 'my-app',
+  directives: [],
+  template: `
+
+  `
+})
+
+export class AppComponent {
+  public property: string ;
+  constructor(){
+    this.property = ""
+  }
+}
